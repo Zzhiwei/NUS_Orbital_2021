4d82b851c4005cd9116097f76824806487176fa7
softmagnet <59001819+softmagnet@users.noreply.github.com>
2021-05-17 17:57:42 +0800

    linked home page with backend

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,16 @@
 
 import { Card, Drawer, Grid, Link, makeStyles, Paper } from '@material-ui/core';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ProjectCard from '../components/ProjectCard';
 import Search from '../components/Search';
 import { db } from '../firebase'
+
+const truncate = (str) => {
+    if (str.length > 300) {
+        return str.substring(0, 280) + " ..."
+    }        
+    return str        
+}
 
 
 const useStyles = makeStyles((theme) => {
@@ -39,6 +46,11 @@ const longText = "I am looking for partner to participate in the upcoming shopee
 function Home() {
     const classes = useStyles();
     const [filter, setFilter] = useState(false);
+    const [posts, setPosts] = useState([]);
+    const [toRender, setToRender] = useState([])
+    
+
+    //for filter pop up
     const toggleFilter = (open) => (event) => {
         if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
             return;
@@ -46,67 +58,53 @@ function Home() {
         setFilter(open)
     }
 
+    //sends query to backend when first mounting
+    useEffect(() => {
+        let received = []
+        db.collection('posts').get().then(snapShot => {
+            snapShot.forEach(doc => {
+                received.push(doc.data())
+            }) 
+            setPosts(received)
+        }).catch(e => {
+            console.log(e)
+        })
+    }, [])
+
     
-    const truncate = (str) => {
-        if (str.length > 300) {
-            return str.substring(0, 280) + " ..."
-        }        
-        return str        
+    
+
+
+    const prepareRender = async () => {
+        let renderList = []
+        for (const post of posts) {
+            const name = await db.collection('users').doc(post.author).get().then(res => {
+                const data = res.data()
+                return data.firstName + ", " + data.lastName
+            })
+            
+            renderList = [...renderList, (
+                <Grid item xs={12} md={6} key={post.id}>
+                    <ProjectCard
+                        title={post.title}
+                        author={name}
+                        description={post.description}
+                        chips={post.skills}
+                    />
+                </Grid>  
+            )]
+           
+        }
+        setToRender(renderList)
     }
 
-    const renderSampleContent = () => {
-        return (
-            <Grid container spacing={3}>
-                    <Grid item xs={12} md={6}>
-                        <ProjectCard
-                            title="Shopee code league"
-                            author="zhiwei007"
-                            description={truncate(longText + longText)}
-                            chips={["data science", "data structures", "algorithms", "programming"]}
-                        />
-                    </Grid>              
-                    <Grid item xs={12} md={6}>
-                        <ProjectCard
-                            title="NUS project orbital"
-                            author="myc37"
-                            description={truncate(shortText)}
-                            chips={["programming", "computer science", "web development", "National University of Singapore"]}
-                        />
-                    </Grid>  
-                    <Grid item xs={12} md={6}>
-                        <ProjectCard
-                            title="24 ACSI short film competition"
-                            author="surendra shenoy basti"
-                            description={truncate("looking for a few people to team up with and participate in the ACSI media club short film competition")}
-                            chips={["short films", "video editing", "film-making"]}
-                        />
-                    </Grid>    
-                    <Grid item xs={12} md={6}>
-                        <ProjectCard
-                            title="facebook hackathon"
-                            author="Wang Gang"
-                            description={truncate("looking for an computer science student to participate in Facebook Hackathon with")}
-                            chips={["programming", "hackathon"]}
-                        />
-                    </Grid>    
-                    <Grid item xs={12} md={6}>
-                        <ProjectCard
-                            title="engineering innovation challenge"
-                            author="Wang Gang"
-                            description={truncate("looking for an engineering student to participate in this competition")}
-                            chips={["engineering"]}
-                        />
-                    </Grid>    
-                    <Grid item xs={12} md={6}>
-                        <ProjectCard
-                            title="NUS Egaming league of legends tournament"
-                            author="noobmaster97"
-                            description={truncate("looking for league of legends players to play in the NUS eGaming tournament")}
-                            chips={["league of legends", "video-games"]}
-                        />
-                    </Grid>                  
-                </Grid> 
-        )
+    /*
+    first render: posts got nth yet
+    2nd render: triggerred by setPosts inside first async func
+    3rd render: triggered by setToRender inside second async func
+    */
+    if (posts.length && !toRender.length) {
+        prepareRender()
     }
 
     return (        
@@ -114,13 +112,11 @@ function Home() {
                 <Search toggleFilter={toggleFilter} />
                 <Drawer anchor="right" open={filter} onClose={toggleFilter(false)}>
                     <Paper style={{height: '1000px', width: '500px', padding: '100px'}}>
-                        
                     </Paper>
                 </Drawer>
-                {renderSampleContent()}   
-                {renderSampleContent()}   
-                {renderSampleContent()}   
-                {renderSampleContent()}   
+                <Grid container spacing={3}>
+                    {toRender}
+                </Grid>
             </div>
         
     );
