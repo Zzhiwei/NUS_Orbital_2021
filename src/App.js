821e4f1614a087b8ee3dd12141c023d9f2fcd80e
softmagnet <59001819+softmagnet@users.noreply.github.com>
2021-05-24 18:22:55 +0800

    added load more

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import EditPost from './pages/EditPost';
 import ForgotPassword from './pages/ForgotPassword';
 import MyPosts from './pages/MyPosts';
 import MyBookmarks from './pages/MyBookmarks';
+import ScrollTest from './pages/ScrollTest';
 
 
 
